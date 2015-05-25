baa824a00794fed66dc105ca6d6ac0a21a094e18
aihui zhu <mr.zhuah@gmail.com>
2015-05-25 12:13:28 +0800

    fixed error test

--- error_test.go
+++ error_test.go
@@ -1,34 +1,17 @@
 package gomodel
 
 import (
-	"database/sql"
 	"testing"
 
 	"github.com/cosiner/gohper/errors"
 	"github.com/cosiner/gohper/testing2"
 )
 
-func TestErrDuplicateKey(t *testing.T) {
+func TestError(t *testing.T) {
 	tt := testing2.Wrap(t)
 	err := errors.Err(`Duplicate entry '14d1b6c34a001-1648e0754a001' for key 'PRIMARY'`) // for combined primary key
-	ErrForDuplicateKey(err, func(key string) error {
-		tt.Eq(key, "PRIMARY")
-		return nil
-	})
-}
+	tt.Eq(PRIMARY_KEY, Error.DuplicateKey(err))
 
-func TestErrForeignKey(t *testing.T) {
-	tt := testing2.Wrap(t)
-	err := errors.Err("CONSTRAINT `article_vote_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`)")
-	ErrForForeignKey(err, func(key string) error {
-		tt.Eq(key, "article_id")
-		return nil
-	})
-}
-
-func TestErrNoRows(t *testing.T) {
-	tt := testing2.Wrap(t)
-	err := errors.Err("Error No Rows")
-
-	tt.Eq(err, ErrForNoRows(sql.ErrNoRows, err))
+	err = errors.Err("CONSTRAINT `article_vote_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`)")
+	tt.Eq("article_id", Error.ForeignKey(err))
 }
